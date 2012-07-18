3d5c2d7e605c3490e735c81fceb85034ed1769d8
Zhang Zhe <bleastrind@gmail.com>
2012-07-18 09:59:19 +0800

    delete some extra lines

--- market/app/controllers/RoutingRecomController.java
+++ market/app/controllers/RoutingRecomController.java
@@ -5,19 +5,15 @@ import java.util.List;
 
 import org.internetrt.sdk.InternetRT;
 import org.internetrt.sdk.util.DescribedListenerConfig;
-import org.internetrt.sdk.util.FreeRoutingGenerator;
 import org.internetrt.sdk.util.ListenerConfig;
 import org.internetrt.sdk.util.FreeRoutingGenerator;
 import org.internetrt.sdk.util.Signal;
 
 import config.properties;
 
-import cn.edu.act.internetos.appmarket.service.TermToJson;
 
 import play.mvc.Before;
 import play.mvc.Controller;
-import play.mvc.Scope.Session;
-import models.App;
 import models.RoutingRecommender;
 class RoutingChoice{
 	private String signalName;
