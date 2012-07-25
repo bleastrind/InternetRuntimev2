94f3f7d8d7683cbcfcfbbd84e8c17ee651a6a912
Zhang Zhe <bleastrind@gmail.com>
2012-07-25 16:17:43 +0800

    Added extendsSignal Listener for market

--- market/app/controllers/AppController.java
+++ market/app/controllers/AppController.java
@@ -62,6 +62,12 @@ public class AppController extends Controller {
 		// TODO:nick name
 		render("AppService/listAllApp.html", applist, token);
 	}
+	
+
+	public static void listAppBySignalName(String signalname){
+		List<App> applist = AppService.getAppsBySignal(signalname);
+		render("AppService/listAllApps.html", applist, true);
+	}
 
 	public static void addUserApp() {
 		List<App> applist = AppService.getAllApps();
