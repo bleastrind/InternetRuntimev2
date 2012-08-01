e57c87f2be8866f67016f75d94475e637edb94a1
wangfeiact <wangfei.wg@gmail.com>
2012-08-01 21:29:53 +0800

    add flow chart in appmarket index.html

--- IfApp/src/config/properties.java
+++ IfApp/src/config/properties.java
@@ -3,8 +3,8 @@ import org.internetrt.sdk.InternetRT;
 import org.internetrt.sdk.InternetRTConfig;
 
 public class properties {
-	public static String appID = "927259c0-1d4b-4170-826a-8dea43a7a7a7";   //canssadra in 3.145 "12dd85a3-5ae1-434b-82ef-26cf5186386f";
-	public static String appSecret = "6337a390-0dda-4b02-9a13-b5fd175bb1c1"; //"ecb54b2b-6c31-41c6-bc77-e8f43115cce2";
+	public static String appID = "3847c19b-bcab-424b-9240-c13e955e7011";   //canssadra in 3.145 "12dd85a3-5ae1-434b-82ef-26cf5186386f";
+	public static String appSecret = "65851627-abbd-455d-91d3-2fb6922632ca"; //"ecb54b2b-6c31-41c6-bc77-e8f43115cce2";
 	static {
 		try {
 			InternetRTConfig config = new InternetRTConfig();
