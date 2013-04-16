3a8e92e15fb8da55524a9b2e79da44ed9f043854
xupeilong <xupeilong@gmail.com>
2013-04-16 19:24:39 +0800

    ClusterManagerImpl.scala

--- InternetRT/app/org/internetrt/core/siblings/ClusterManagerImpl.scala
+++ InternetRT/app/org/internetrt/core/siblings/ClusterManagerImpl.scala
@@ -5,15 +5,11 @@ abstract class ClusterManagerImpl extends ClusterManager {
 
   val DHTRing = List(
     (0, NodeRef.getNode("192.168.3.145")),
-    (10768, NodeRef.getNode("192.168.3.155")),
-    (2754673, NodeRef.getNode("192.168.3.234")),
-    (275467373, NodeRef.getNode("192.168.3.123")),
-    (1725467373, NodeRef.getNode("192.168.3.122")),
-    (2054636373, NodeRef.getNode("192.168.3.121"))
+    (493379200, NodeRef.getNode("192.168.3.160"))
     );
 
   def getNodeRef(uid: String): Option[NodeRef] = {
-    val desiredNode: NodeRef = DHTRing.filter(_._1 < uid.hashCode()).headOption match {
+    val desiredNode: NodeRef = DHTRing.filter(_._1 > uid.hashCode()).headOption match {
       case Some((i, n)) => n
       case None => DHTRing.head._2
     }
